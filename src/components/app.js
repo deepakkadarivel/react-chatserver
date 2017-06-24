e0062f2fc9b80aeecc9c2668bbb5b6e108bb650c
Deepakkv <deepakkv@in-deepakkv.local>
2017-06-24 13:36:57 +0530

    [Deepak] OAuth simple login popup implementation

--- src/components/app.js
+++ src/components/app.js
@@ -1,11 +1,27 @@
 import React, { Component } from 'react'
 import ChatRoom from './ChatRoom'
 
+import Auth from '../Auth/Auth'
+
 class App extends Component {
+
+  constructor() {
+    super()
+    this.state = {
+      auth: new Auth(),
+    }
+    this.login = this.login.bind(this)
+  }
+
+  login() {
+    this.state.auth.login()
+  }
+
   render() {
     return (
       <div>
         <ChatRoom />
+        <button onClick={this.login}>Login</button>
       </div>
     )
   }
